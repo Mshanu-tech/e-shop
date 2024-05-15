a874370b671f8837df796e918853ca8030defd6c
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-05-15 17:16:00 +0530

    working in user home session

--- frontend/src/Pages/User/Home/index.jsx
+++ frontend/src/Pages/User/Home/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './style.css'
 import Navbar from '../../../components/navbar/Navbar'
 
 function Home() {
   return (
-    <div><Navbar/></div>
+    <>
+    <Navbar/>
+    <div className="main-container">
+      <div className="container">
+        <div className="row">
+          <div className='home-leftside'>
+<h1>50% OFFER</h1>
+          </div>
+          <div className="home-rightside">
+            <img src="/Image/bn-removebg-preview.png" alt="" />
+
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
