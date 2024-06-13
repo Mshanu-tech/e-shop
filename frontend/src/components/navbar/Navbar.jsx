39242a8250e57acbc215ad530f1dbc6b0734f485
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-06-13 21:36:53 +0530

    update navbar and home front page

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,16 +6,22 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import WorkIcon from '@mui/icons-material/Work';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import Badge from '@mui/material/Badge';
+import CategoryIcon from '@mui/icons-material/Category';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 function Navbar() {
     const [userNavbar, setUserNavbar] = useState(true);
     const [navValue, setNavValue] = useState(0);
+    const [dress, setDress] = useState(true)
     const handleUserNavbar = () => {
         setUserNavbar(prevState => !prevState);
     };
-console.log(navValue);
+    const handleDress = () => {
+        setDress(prevState => !prevState)
+    };
+
+
     return (
         <>
             <div className="main_nav_container">
@@ -49,7 +55,8 @@ console.log(navValue);
                                 </div>
 
                                 <div className="navbar_user_side">
-                                    <a onClick={handleUserNavbar} href="#"><PersonIcon className="search-icon" /></a>
+                                    <BottomNavigationAction onClick={handleUserNavbar} label="Profile" icon={<PersonIcon />} />
+                                    <BottomNavigationAction onClick={handleDress} label="Dress" icon={<CategoryIcon />} />
                                     <ul className="navbar_user_profile" style={{ display: userNavbar ? 'block' : 'none' }}>
                                         <li><PersonIcon className="search-icon" /></li>
                                         <li><FavoriteOutlinedIcon className="search-icon" /></li>
@@ -61,6 +68,46 @@ console.log(navValue);
                                         </li>
                                     </ul>
                                 </div>
+
+                                <div className='dressCategory' style={{ display: dress ? 'flex' : 'none'}}>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="https://www.shutterstock.com/image-vector/sport-tshirt-icon-training-sign-260nw-1267243321.jpg" alt="" />
+                                        {/* <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>T-Shirt</p> */}
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="https://cdn4.vectorstock.com/i/1000x1000/30/48/shirt-icon-vector-22623048.jpg" alt="" />
+                                        {/* <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>Shirt</p> */}
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="https://static.vecteezy.com/system/resources/previews/010/347/283/non_2x/pants-boy-garment-line-icon-illustration-vector.jpg" alt="" />
+                                        {/* <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>Pant</p> */}
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="https://cdn-icons-png.flaticon.com/512/4795/4795378.png" alt="" />
+                                        {/* <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>Watch</p> */}
+                                    </div>
+                                    {/* <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="/Image/shoe.png" alt="" />
+                                        <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>Shoe</p>
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%" }} src="/Image/shorts.webp" alt="" />
+                                        <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>shorts</p>
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%" }} src="/Image/chappal.webp" alt="" />
+                                        <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>chappal</p>
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%", background: "white" }} src="/Image/wallet.png" alt="" />
+                                        <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>wallet</p>
+                                    </div>
+                                    <div style={{ cursor: "pointer" }}>
+                                        <img style={{ width: "50px", height: "50px", borderRadius: "50%" }} src="/Image/innerwear.webp" alt="" />
+                                        <p style={{ fontWeight: "bold", fontSize: "13px", textAlign: "center" }}>Innerwear</p>
+                                    </div> */}
+                                </div>
+
                             </nav>
                         </div>
                     </div>
