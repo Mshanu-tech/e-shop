3cecf79bfb58ad56b9865dae1dbf9633f5d36766
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2024-06-13 21:07:50 +0530

    update user navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,13 +6,16 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import WorkIcon from '@mui/icons-material/Work';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import Badge from '@mui/material/Badge';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 function Navbar() {
     const [userNavbar, setUserNavbar] = useState(true);
+    const [navValue, setNavValue] = useState(0);
     const handleUserNavbar = () => {
         setUserNavbar(prevState => !prevState);
     };
-
+console.log(navValue);
     return (
         <>
             <div className="main_nav_container">
@@ -27,19 +30,24 @@ function Navbar() {
                                     <input className="searchinput" type="text" placeholder="Search...." />
                                     <a href="#"><SearchIcon className="search-icon" style={{ paddingTop: "6px" }} /></a>
                                 </div>
+                                <div className='navbar_user'>
+                                    <BottomNavigation
+                                        showLabels
+                                        value={navValue}
+                                        onChange={(event, newValue) => {
+                                            setNavValue(newValue);
+                                        }}
+                                    >
+                                        <BottomNavigationAction label="Favorites" icon={<FavoriteOutlinedIcon />} />
+                                        <BottomNavigationAction label="Work" icon={<WorkIcon />} />
+                                        <BottomNavigationAction
+                                            label="Cart"
+                                            icon={<Badge badgeContent={4} color="primary"><ShoppingCartIcon /></Badge>}
+                                        />
+                                        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+                                    </BottomNavigation>
+                                </div>
 
-                                <ul className="navbar_user">
-                                    <li><a href="#"><FavoriteOutlinedIcon className="search-icon" /></a></li>
-                                    <li><a href="#"><WorkIcon className="search-icon" /></a></li>
-                                    <li className="checkout">
-                                        <a href="#">
-                                            <Badge badgeContent={4} color="primary">
-                                                <ShoppingCartIcon className="search-icon" />
-                                            </Badge>
-                                        </a>
-                                    </li>
-                                    <li><a href="#"><PersonIcon className="search-icon" /></a></li>
-                                </ul>
                                 <div className="navbar_user_side">
                                     <a onClick={handleUserNavbar} href="#"><PersonIcon className="search-icon" /></a>
                                     <ul className="navbar_user_profile" style={{ display: userNavbar ? 'block' : 'none' }}>
@@ -58,6 +66,8 @@ function Navbar() {
                     </div>
                 </div>
             </div>
+
+
         </>
     );
 }
